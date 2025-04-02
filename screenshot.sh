968a4793b4ac7ebda758e8508f1188d944ca1ba3
kevwargo <jarasz.kevwargo@gmail.com>
2025-04-02 22:40:20 +0200

    Fix screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-dir="${1:-/mnt/other/screens}"
-window="${2:-$(wmiface activeWindow)}"
+set -e
 
-[ -z "$window" ] && exit 1
+dir=$(realpath ~/screens)
+name=screen_$(date "+%Y-%m-%d_%H-%M-%S.%6N%z").png
+path="$dir/$name"
 
-last=$(ls "$dir" | grep -E "^screen[0-9]{5}\.png$" | sort | tail -1)
-import -window $window $dir/screen$(printf "%.5d" $(( 10#${last:6:5} + 1 ))).png
+mkdir -p $dir
+import "$@" "$path"
+
+echo "$path"
