edb022f216a54e5c5f330e078bbccde8c7a025d0
kevwargo <jarasz.kevwargo@gmail.com>
2025-04-02 22:39:00 +0200

    Remove unnecessary exports and fix indentation

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -2,12 +2,12 @@
 
 ps1_newline_on()
 {
-    export ps1_part_2="$(echo "$ps1_part_2" | sed 's/^\\\[/\\n \\[/')"
+    ps1_part_2="$(echo "$ps1_part_2" | sed 's/^\\\[/\\n \\[/')"
 }
 
 ps1_newline_off()
 {
-    export ps1_part_2="$(echo "$ps1_part_2" | sed 's/^\\n //')"
+    ps1_part_2="$(echo "$ps1_part_2" | sed 's/^\\n //')"
 }
 
 if [ $UID == 0 ]; then
@@ -16,6 +16,3 @@ else
     ps1_part_1='\[\033[01;32m\]\h\[\033[01;34m\] \w\[\033[00m\]'
 fi
 ps1_part_2='\[\033[01;34m\]\$\[\033[00m\] '
-
-export PS1="$ps1_part_1 $ps1_part_2"
-
